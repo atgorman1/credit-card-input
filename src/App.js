46b9c16d5fb94a079d82ece90e594166bbdd4314
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-08-09 11:21:05 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import bgDesktop from "./images/bg-main-desktop.png";
+import bgMobile from "./images/bg-main-mobile.png";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        {/* Background Images - picture tag allows to switch images based on screen size*/}
+        <div className="absolute -z-10">
+          <picture>
+            <source media="(min-width: 1024px)" srcSet={bgDesktop} />
+            <img src={bgMobile} alt="purple gradiated background" />
+          </picture>
+        </div>
+
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+          <div>
+            <article className="cc-front">
+              {/* Front CC Image */}Front CC
+            </article>
+            <article className="cc-back">{/* Back CC Image */}Back CC</article>
+          </div>
+
+          <div>{/* CC Form */}Form</div>
+        </div>
+      </section>
+    </>
   );
 }
-
-export default App;
